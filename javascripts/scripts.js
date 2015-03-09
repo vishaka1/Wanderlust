c8094f080328b26b8ed1f254cfe3c15c50e5db13
vishaka1 <vishaka1@e.ntu.edu.sg>
2015-03-09 20:57:24 +0800

    chagde

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -10,22 +10,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function drawMap () {
   var canvasTiles = L.tileLayer.canvas();
-  var map = L.mapbox.map('map', 'vishaka1.6efc031c').setView([19.1562, 72.7631], 11);
+  var map = L.mapbox.map('map', 'vishaka1.6efc031c').setView([1.3526, 103.7988,], 11);
 
   L.canvasOverlay().drawing(drawPoints).addTo(map);
 }
 
 function drawPoints (canvasOverlay, params) {
   var ctx = params.canvas.getContext('2d');
+  var radius = 20;
   
   ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
   
   ctx.beginPath();
   points.map(function (d, i) {
     dot = canvasOverlay._map.latLngToContainerPoint( [d[0], d[1]] );
-    ctx.lineTo(dot.x, dot.y);
+    // ctx.lineTo(dot.x, dot.y);
+
+
+    ctx.arc(dot.x, dot.y, radius, 0, 2 * Math.PI, false);
+
   });
-   // set line color
+  /* // set line color
       ctx.strokeStyle = '#7c3801';
+      ctx.stroke();*/
+
+      ctx.fillStyle = 'green';
+      ctx.fill();
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = '#003300';
       ctx.stroke();
-}
+      }
